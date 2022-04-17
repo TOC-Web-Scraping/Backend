c1b1d2dfda060ba3bf5e614a4a073ef425f0976f
pjpure <pure254326@gmail.com>
2022-04-17 23:09:59 +0700

    updaate search teams

--- src/controllers/team.controller.ts
+++ src/controllers/team.controller.ts
@@ -4,9 +4,20 @@ import fetch from 'cross-fetch';
 
 async function getAllTeams(req: Request, res: Response) {
     try {
-        const response = await fetch('https://toc-web-scraping.github.io/scraping/data/teams.json');
-        const teams: Team[] = await response.json();
-        res.status(200).send(teams);
+        const search: string = String(req.query.search);
+        if (!search) {
+            const response = await fetch('https://toc-web-scraping.github.io/scraping/data/teams.json');
+            const teams: Team[] = await response.json();
+            res.status(200).send(teams);
+        } else {
+            const response = await fetch('https://toc-web-scraping.github.io/scraping/data/teams.json');
+            const teams: Team[] = await response.json();
+            const filteredTeams = teams.filter((team) => {
+                return team.name.toLowerCase().includes(search.toLowerCase()) || team.region.toLowerCase().includes(search.toLowerCase()) || team.location.toLowerCase().includes(search.toLowerCase());
+            });
+            res.status(200).send(filteredTeams);
+        }
+
     } catch (err: any) {
         res.status(500).json({ message: err.message });
     }
@@ -42,7 +53,9 @@ async function getTeamById(req: Request, res: Response) {
     }
 }
 
+
 export default {
     getAllTeams,
-    getTeamById
+    getTeamById,
+
 }
