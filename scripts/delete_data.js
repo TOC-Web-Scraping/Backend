de5ffcdd7f7e030e229d8b708d81eae74928bbf7
chuckboliver <patpumhak@gmail.com>
2022-04-26 12:01:09 +0700

    add drop collection script

---
+++ scripts/delete_data.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+const { MongoClient } = require('mongodb');
+
+dotenv.config();
+
+const { DATABASE_NAME, MONGODB_URI } = process.env;
+
+console.log('Connecting mongodb...');
+MongoClient.connect(MONGODB_URI, async (err, client) => {
+  if (err) throw err;
+
+  const db = client.db(DATABASE_NAME);
+
+  await db.collection('players').drop();
+  console.log('Delete players data successful');
+
+  await db.collection('teams').drop();
+  console.log('Delete teams data successful');
+
+  await db.collection('matchs').drop();
+  console.log('Delete matchs data successful');
+
+  await db.collection('agents').drop();
+  console.log('Delete agents data successful');
+
+  await client.close();
+  console.log('Close database connection...');
+});
