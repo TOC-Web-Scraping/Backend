ad1fe72cb77f1be6226461561c14eb631b934a63
chuckboliver <patpumhak@gmail.com>
2022-04-26 12:00:50 +0700

    create data script is work

--- scripts/create_data.js
+++ scripts/create_data.js
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv';
-import { MongoClient } from 'mongodb';
-import fetch from 'cross-fetch';
+const dotenv = require('dotenv');
+const { MongoClient } = require('mongodb');
+const fetch = require('cross-fetch');
 
 dotenv.config();
 
@@ -9,6 +9,8 @@ const TEAM_URL = 'https://raw.githubusercontent.com/TOC-Web-Scraping/scraping/ma
 const MATCH_URL = 'https://raw.githubusercontent.com/TOC-Web-Scraping/scraping/main/data/matchs.json';
 const AGENT_URL = 'https://raw.githubusercontent.com/TOC-Web-Scraping/scraping/main/data/agents.json';
 
+const { DATABASE_NAME, MONGODB_URI } = process.env;
+
 const getData = async (url) => {
   const response = await fetch(url);
   const result = await response.json();
@@ -16,22 +18,27 @@ const getData = async (url) => {
 };
 
 console.log('Connecting mongodb...');
-MongoClient.connect(process.env.MONGODB_URI, async (err, db) => {
+MongoClient.connect(MONGODB_URI, async (err, client) => {
   if (err) throw err;
 
+  const db = client.db(DATABASE_NAME);
+
   const players = await getData(PLAYER_URL);
-  db.collection('players').insertMany(players);
+  await db.collection('players').insertMany(players);
   console.log('Insert players data successful');
 
   const teams = await getData(TEAM_URL);
-  db.collection('teams').insertMany(teams);
+  await db.collection('teams').insertMany(teams);
   console.log('Insert teams data successful');
 
   const matchs = await getData(MATCH_URL);
-  db.collection('matchs').insertMany(matchs);
+  await db.collection('matchs').insertMany(matchs);
   console.log('Insert matchs data successful');
 
   const agents = await getData(AGENT_URL);
-  db.collection('agents').insertMany(agents);
+  await db.collection('agents').insertMany(agents);
   console.log('Insert agents data successful');
+
+  await client.close();
+  console.log('Close database connection...');
 });
