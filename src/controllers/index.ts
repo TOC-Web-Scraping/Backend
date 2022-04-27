d1f5f23d4093a2c1b02ab123ff70a8ca7a811e6c
Sukrit22 <popyoy7@gmail.com>
2022-04-27 19:29:16 +0700

    added Map Support for query
    added map query support for all the 3 main modules, models, controllers and routes.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
 export { default as TeamController } from './team.controller';
 export { default as PlayerController } from './player.controller';
 export { default as AgentController } from './agent.controller';
+export { default as ValoMapController } from './valomap.controller';
