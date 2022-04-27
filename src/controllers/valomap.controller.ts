eb1cd2421f8810d3047912505fd5b77d5b64a7fe
chuckboliver <patpumhak@gmail.com>
2022-04-27 21:10:07 +0700

    merge

--- src/controllers/valomap.controller.ts
+++ src/controllers/valomap.controller.ts
@@ -3,45 +3,46 @@ import { cache } from '../middlewares/cache';
 import { ValoMap } from '../models';
 
 interface ValoMapFilters {
-    mapName?: string;
-    country?: string;
-    bombSites?: string;
-    teleporters?: string;
+  mapName?: string;
+  country?: string;
+  bombSites?: string;
+  teleporters?: string;
 }
 
 async function getValoMap(req: Request, res: Response) {
-    try {
-        const pageID = req.query.pageID as string;
-        const pageSize = req.query.pageSize as string;
-        const mapName = req.query.mapName as string;
-        const country = req.query.country as string;
-        const bombSites = req.query.bombSites as string;
-        const teleporters = req.query.teleporters as string;
-        const filterValues: ValoMapFilters = { mapName, country, bombSites, teleporters };
-        const filters: ValoMapFilters = {};
+  try {
+    const pageID = req.query.pageID as string;
+    const pageSize = req.query.pageSize as string;
 
-        Object.keys(filterValues).forEach((key) => {
-            const tkey = key as keyof ValoMapFilters;
-            if (filterValues[tkey]) {
-                filters[tkey] = filterValues[tkey];
-            }
-        });
+    const mapName = req.query.mapName as string;
+    const country = req.query.country as string;
+    const bombSites = req.query.bombSites as string;
+    const teleporters = req.query.teleporters as string;
+    const filterValues: ValoMapFilters = { mapName, country, bombSites, teleporters };
+    const filters: ValoMapFilters = {};
 
-        let query = ValoMap.find(filters);
-        if (pageID && pageSize) {
-            query = query.skip((+pageID - 1) * +pageSize).limit(+pageSize);
-        }
-        const result = await query;
-        const jsonResult = result.map((r) => r.toJSON());
+    Object.keys(filterValues).forEach((key) => {
+      const tkey = key as keyof ValoMapFilters;
+      if (filterValues[tkey]) {
+        filters[tkey] = filterValues[tkey];
+      }
+    });
 
-        cache.set(req.originalUrl, jsonResult, 60);
-
-        res.status(200).json(jsonResult);
-    } catch (err: any) {
-        res.status(500).json({message: err.message});
+    let query = ValoMap.find(filters);
+    if (pageID && pageSize) {
+      query = query.skip((+pageID - 1) * +pageSize).limit(+pageSize);
     }
+    const result = await query;
+    const jsonResult = result.map((r) => r.toJSON());
+
+    cache.set(req.originalUrl, jsonResult, 60);
+
+    res.status(200).json(jsonResult);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
 }
 
 export default {
-    getValoMap,
+  getValoMap,
 };
--- src/models/valomap.model.ts
+++ src/models/valomap.model.ts
@@ -3,21 +3,21 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const valoMapSchema = new Schema({
-    mapName: String,
-    country: String,
-    releaseDate: String,
-    bombSites: String,
-    teleporters: String,
-    imageUrl: String
+  mapName: String,
+  country: String,
+  releaseDate: String,
+  bombSites: String,
+  teleporters: String,
+  imageUrl: String,
 });
 
 export type ValoMapDocument = mongoose.Document & {
-    mapName: String;
-    country: String;
-    releaseDate: String;
-    bombSites: String;
-    teleporters: String;
-    imageUrl: String;
+  mapName: String;
+  country: String;
+  releaseDate: String;
+  bombSites: String;
+  teleporters: String;
+  imageUrl: String;
 };
 
 export const ValoMap = mongoose.model<ValoMapDocument>('ValoMap', valoMapSchema);
