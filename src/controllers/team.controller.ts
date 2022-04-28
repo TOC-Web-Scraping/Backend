31dc93ece47c2207861a19beb3c79fea41a14e36
Sukrit22 <popyoy7@gmail.com>
2022-04-28 21:51:16 +0700

    backup before live share

--- src/controllers/team.controller.ts
+++ src/controllers/team.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { FilterQuery } from 'mongoose';
-import { Team } from '../models';
+import { Team , Match } from '../models';
 import { cache } from '../middlewares/cache';
 
 interface TeamFilters {
@@ -44,7 +44,8 @@ async function getTeams(req: Request, res: Response) {
 
 async function getTeamById(req: Request, res: Response) {
   try {
-    const team = await Team.aggregate([
+    const teamNameFromUrlUnAwait = Team.findOne({url:req.params.id},{name:1,_id:0});
+    const teamUnAwait = Team.aggregate([
       {
         $match: {
           url: req.params.id,
@@ -60,14 +61,56 @@ async function getTeamById(req: Request, res: Response) {
           foreignField: 'team',
           as: 'players',
         },
+      },/* แบบยาก
+      {
+        $lookup: {
+          localField: 'name',
+          from: 'matches',
+          foreignField: 'team1',
+          as: 'winLose1',
+        }
       },
       {
+        $lookup: {
+          localField: 'name',
+          from: 'matches',
+          foreignField: 'team2',
+          as: 'winLose2',
+        },
+      },*/
+      {
         $project: {
-          'players._id': 0,
+          'players._id': 0,/* แบบยาก
+          'winLose1._id': 0,
+          'winLose1.player':0,
+          'winLose1.date':0,
+          'winLose1.tournament':0,
+          'winLose1.map':0,
+          'winLose1.kill':0,
+          'winLose1.death':0,
+          'winLose1.assist':0,
+          'winLose1.team1':0,
+          'winLose1.team2':0,
+          'winLose1.agents1':0,
+          'winLose1.agents2':0,
+          'winLose2._id': 0,
+          'winLose2.player':0,
+          'winLose2.date':0,
+          'winLose2.tournament':0,
+          'winLose2.map':0,
+          'winLose2.kill':0,
+          'winLose2.death':0,
+          'winLose2.assist':0,
+          'winLose2.team1':0,
+          'winLose2.team2':0,
+          'winLose2.agents1':0,
+          'winLose2.agents2':0,*/
         },
       },
     ]);
-
+    const teamNameFromUrl = await teamNameFromUrlUnAwait;
+    //const matchUnAwait = Match.find({'team1':});
+    const team = await teamUnAwait;
     if (team.length > 0) {
       cache.set(req.originalUrl, team[0], 60);
       res.status(200).json(team[0]);
@@ -77,6 +120,7 @@ async function getTeamById(req: Request, res: Response) {
   } catch (err: any) {
     res.status(500).json({ message: err.message });
   }
+  console.log('teamByIDisRequested');
 }
 
 export default {
